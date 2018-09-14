3ee491a2229f7f325011e01a8e971baecf234ac0
Tobias Brunner <tobias@strongswan.org>
2018-09-14 17:13:09 +0200

    leak-detective: Whitelist additional OpenSSL functions used by libcurl

--- src/libstrongswan/utils/leak_detective.c
+++ src/libstrongswan/utils/leak_detective.c
@@ -582,6 +582,9 @@ static char *whitelist[] = {
 	"OPENSSL_init_crypto",
 	"CRYPTO_THREAD_lock_new",
 	"ERR_add_error_data",
+	"ERR_set_mark",
+	"ENGINE_load_builtin_engines",
+	"CONF_modules_load_file",
 	/* OpenSSL libssl */
 	"SSL_COMP_get_compression_methods",
 	/* NSPR */
